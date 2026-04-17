1eac2fa80132ab7cbadbc7b0fc7628fcd2ad14f9
nabilbaroq <nabilmuhamad89@gmail.com>
2026-04-17 14:27:14 +0700

    feat: add API functions

---
+++ src/lib/api.ts
@@ -0,0 +1,27 @@
+const BASE_URL = "http://localhost:3001"
+
+export async function getProdut() {
+    const res = await fetch(`${BASE_URL}/products`)
+    return res.json()
+}
+
+export async function addProduct(data: object) {
+    const res = await fetch(`${BASE_URL}/products`, {
+        method: "POST",
+        headers: {"Content-Type": "applicant/json"},
+        body: JSON.stringify(data),
+    })
+    return res.json()
+}
+
+export async function deleteProduct(id: number) {
+    const res = await fetch(`${BASE_URL}/products/${id}`, {
+        method: "DELETE"
+    })
+    return res.ok
+}
+
+export async function getCategory() {
+    const res = await fetch(`${BASE_URL}/categories`)
+    return res.json()
+}
